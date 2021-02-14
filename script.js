3b21e647ab2b5a1eab7967aa96a99280e5607bb6
Victoria <vigi2586@stud.kea.dk>
2021-02-14 16:50:45 +0100

    Done finally

---
+++ script.js
@@ -0,0 +1,64 @@
+
+"use strict";
+
+document.addEventListener("DOMContentLoaded", ListAllStudents);
+
+let jsonObject;
+
+// -------------------------------------------------------------------------------------------------------- //
+
+// fetch info from json
+async function ListAllStudents() {
+  console.log("Done");
+
+  const AllStudents = [];
+
+  const url = "https://petlatkea.dk/2021/hogwarts/students.json";
+
+  const jsonData = await fetch(url);
+
+  jsonObject = await jsonData.json();
+
+  ShowStudents(jsonObject);
+};
+
+// -------------------------------------------------------------------------------------------------------- //
+
+function ShowStudents(jsonObject) {
+
+    //console.log("There is so manny");
+
+    jsonObject.forEach((student) => {
+
+      // make template
+      const studenttemp = {
+        firstname: "",
+        middlename: undefined,
+        lastname: "",
+        nickname: undefined,
+        gender: "",
+        img: undefined,
+        house: ""
+    };
+
+      // create template
+      const students = Object.create(studenttemp);
+
+      // ---------------------------------------------------------------------------------------------------- //
+
+      // tells what info should be where and how to get info 
+      const firstSpace = student.fullname.trim().indexOf(" ");
+      const lastSpace = student.fullname.trim().lastIndexOf(" ");
+
+      students.firstname = student.fullname.substring(0, firstSpace).trim();
+      students.middlename = student.fullname.substring(firstSpace, lastSpace).trim();
+      students.lastname = student.fullname.substring(lastSpace).trim();
+      students.nickname = student.fullname.substring(firstSpace, lastSpace).trim();
+      students.gender = student.gender;
+      students.img = student["none"];
+      students.house = student.house;
+
+      console.log(students);
+
+    });
+};
